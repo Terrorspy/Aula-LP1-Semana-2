7d2ee94864213f5df3b3092cd94a4a8ebab275db
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-03 15:08:23 +0000

    Volume e Área de Superfície do Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Altura do Cilindro:");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Raio do Cilindro:");
+            double Raio = Convert.ToDouble(Console.ReadLine());
+
+            double Volume = Math.PI * (Raio * Raio) * Altura;
+            Console.WriteLine("Volume do Cilindro: " + Volume);
+
+            double ÁreaDeSuperfície = 2 * Math.PI * Raio * (Altura + Raio);
+
+            Console.WriteLine("Área de Superfície do Cilindro:" + ÁreaDeSuperfície);
+
+
+        }
+    }
+}
