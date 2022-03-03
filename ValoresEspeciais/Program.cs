752aefbb83252f9dfd18cd2b016192afd67b3fc6
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-03 15:54:21 +0000

    ValoresEspeciais Max e Min

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            float maxFloat = float.MaxValue;
+            float minFloat = float.MinValue;
+
+            Console.WriteLine(maxFloat);
+            Console.WriteLine(minFloat);
+            Console.WriteLine(maxInt);
+            Console.WriteLine(minInt);
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+        }
+    }
+}
