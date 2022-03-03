85535a821ddbeb8bb1078d18b6bb8aeff5ea7c03
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-03 14:23:52 +0000

    Operations

---
+++ IncDec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = 5, b = -7, c;
+           c = ++a + b++;
+
+           int d = 9, e = 2, f;
+           f = --d + --e;
+
+           Console.WriteLine(f);
+           Console.WriteLine(c);
+
+        }
+    }
+}
