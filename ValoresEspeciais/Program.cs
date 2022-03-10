074e58b8a393754340963f6e50f50f97b4b76520
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-10 10:12:27 +0000

    ValoresEspeciais

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -12,13 +12,27 @@ namespace ValoresEspeciais
             int minInt = int.MinValue;
             float maxFloat = float.MaxValue;
             float minFloat = float.MinValue;
+            sbyte sb = sbyte.MaxValue;
+            sbyte sa = sbyte.MinValue;
+            ulong lu = ulong.MinValue;
+            ulong ul = ulong.MaxValue;
+            double x = double.PositiveInfinity;
+            double y = double.NegativeInfinity;
+            float z = float.NaN;
 
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
             Console.WriteLine(maxFloat);
-            Console.WriteLine(minFloat);
+            Console.WriteLine(minFloat);    
             Console.WriteLine(maxInt);
             Console.WriteLine(minInt);
             Console.WriteLine(max);
             Console.WriteLine(min);
+            Console.WriteLine(sa);
+            Console.WriteLine(sb);
+            Console.WriteLine(lu);
+            Console.WriteLine(ul);
         }
     }
 }
